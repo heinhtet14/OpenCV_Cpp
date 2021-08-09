db98a42ebd74d2f6bcf7d81b4080a6d5b685e249
heinhtet14 <heinhtet.1405@gmail.com>
2021-08-09 20:52:12 +0630

    august9th

---
+++ chapter2/display_image/CMakeFiles/display_img.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/display_img.dir/display_img.cpp.o"
+  "display_img"
+  "display_img.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/display_img.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
