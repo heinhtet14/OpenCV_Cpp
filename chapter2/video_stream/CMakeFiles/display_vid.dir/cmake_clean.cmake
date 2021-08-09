67d8462ec5287bf8c5069751ccf196179da78939
heinhtet14 <heinhtet.1405@gmail.com>
2021-08-09 21:31:44 +0630

    video

---
+++ chapter2/video_stream/CMakeFiles/display_vid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/display_vid.dir/display_vid.cpp.o"
+  "display_vid"
+  "display_vid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/display_vid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
