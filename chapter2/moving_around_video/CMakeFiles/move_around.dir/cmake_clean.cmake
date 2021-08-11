581722712beb59cac6ee8d3040139cdff2c4557f
heinhtet14 <heinhtet.1405@gmail.com>
2021-08-11 21:15:39 +0630

    slider

---
+++ chapter2/moving_around_video/CMakeFiles/move_around.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move_around.dir/move_around.cpp.o"
+  "move_around"
+  "move_around.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move_around.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
